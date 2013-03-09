2d7165aaf26a78f3268d9b9b3029dd0ced85d875
A. Schefer/n <bobig12@web.de/n>
2013-03-09 16:17:04 +0100

    Projektarbeit III

---
+++ EventOrg/NeuesProjekt.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace EventOrg
+{
+    public partial class NeuesProjekt : PhoneApplicationPage
+    {
+        public NeuesProjekt()
+        {
+            InitializeComponent();
+        }
+
+        private void but_erstellen_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/EventGuide.xaml", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
