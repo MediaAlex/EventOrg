cd64ab975d8866d7d16781e04cd23e1d668d29a7
derpascal <tehasd@web.de>
2013-04-15 11:48:46 +0200

    !

--- EventOrg/Kunde.cs
+++ EventOrg/Kunde.cs
@@ -12,10 +12,44 @@ using System.Windows.Shapes;
 namespace EventOrg
 {
     public class Kunde
-    {
-        public string kunde_vorname { get; set; }
-        public string kunde_nachname { get; set; }
-        public string kunde_adresse { get; set; }
-        public int kunde_telefon { get; set; }
+    { 
+        public string vorname;
+        public string nachname;
+        public string strasse;
+        public string ort;
+        public int telefon;
+        public int KundenID;
+
+        public string _Vorname
+        {
+            get { return vorname; } 
+            set { vorname = value; }
+        }
+        public string _Nachname
+        {
+            get { return nachname; }
+            set { nachname = value; }
+        }
+        public int _Telefon
+        {
+            get { return telefon; }
+            set { telefon = value; }
+        }
+        public string _Strasse
+        {
+            get { return strasse; }
+            set { strasse = value; }
+        }
+        public string _Ort
+        {
+            get { return ort; }
+            set { ort = value; }
+        }
+        public int _KundenID
+        {
+            get { return KundenID; }
+            set { KundenID = value; }
+        }
     }
+    
 }
